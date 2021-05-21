22aca4ea793ae940a2f22c3e05aeb5d2e0479393
Nate Steil <nathanrsteil@gmail.com>
2021-05-20 21:02:04 -0500

    completed the chapter 6 studio without bonus mission

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,58 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void bracketNestIsTrue(){
 
+        assertTrue(BalancedBrackets.hasBalancedBrackets("({[{}]})"));
+    }
+
+    @Test
+    public void bracketsNestIsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{{[}}}"));
+    }
+
+    @Test
+    public void twoBalancedWords(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Banana] [Bread]"));
+    }
+
+    @Test public void twoWordsDifferntBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{Pancake} [Waffle]"));
+    }
+
+    @Test
+    public void facingAway(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]][["));
+    }
+
+    @Test
+    public void noBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("no brackets"));
+    }
+
+    @Test
+    public void bracketsConcatenated(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[this is test" + " 8 billion]]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void concatAPair(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[" + " ]"));
+    }
+
+    @Test
+    public void subSomeCurlys(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]{]"));
+    }
+
+    @Test
+    public void lotsOfBracketPairs() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[This is] m[y long]est tes[t] yet [but] i th[in]k its fine[]"));
+    }
 }
